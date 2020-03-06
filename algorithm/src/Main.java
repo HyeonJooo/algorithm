9d6041cacf3bdfb235199c68c38b3a261e386849
parkh <parkh@DESKTOP-5PT82F9>
2020-03-06 13:59:50 +0900

    최초업로드

---
+++ algorithm/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] pack = new int[m];
+        int[] one = new int[m];
+        for(int i = 0 ; i<m; i++) {
+        	pack[i] = sc.nextInt();
+        	one[i] = sc.nextInt();
+        }
+        Arrays.sort(pack);
+        Arrays.sort(one);
+        
+        if(n<=6) 
+        	System.out.println(Math.min(pack[0], one[0]*n));
+        else
+        	System.out.println(Math.min(Math.min(pack[0]*(n/6) + one[0]*(n%6), one[0]*n),pack[0]*(n/6+1)));        
+    }
+}
