b057aa6d7a372a2fb5991d84ac327648bce51da7
HyeonJooo <58289478+HyeonJooo@users.noreply.github.com>
2020-03-08 21:09:33 +0900

    피보나치 수2

---
+++ BAEKJOON/기타/2748.java
@@ -0,0 +1,20 @@
+//백준 알고리즘 2748번 : 피보나치 수2
+import java.io.*;
+import java.util.*;
+public class Main {   
+    public static void main(String[] args) throws IOException {
+       Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       long a = 0;
+       long b = 1;
+       long c = 0;
+       
+       for(int i = 0; i<n; i++) {
+    	   c = a+b;
+    	   b = a;
+    	   a = c;
+       }
+       
+       System.out.println(c);
+    }
+}
