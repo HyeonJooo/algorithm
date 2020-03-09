a79e80b94432fbafede9fb68d79fbeea1475621d
HyeonJooo <58289478+HyeonJooo@users.noreply.github.com>
2020-03-09 12:36:13 +0900

    Update 11944.java

--- BAEKJOON/알고리즘대회/고려대학교/11944.java
+++ BAEKJOON/알고리즘대회/고려대학교/11944.java
@@ -1,6 +1,5 @@
 //2016 고려대학교 프로그래밍 경시대회 3번
 //백준 알고리즘 11943번 : NN
-
 import java.io.*;
 import java.util.*;
 public class Main {
@@ -8,13 +7,16 @@ public class Main {
     	Scanner sc = new Scanner(System.in);
     	int n = sc.nextInt();
     	int m = sc.nextInt();
-    	if(n+n > m) {
+    	int length = String.valueOf(n).length();
+    	
+    	if(length*n > m) {
     		String[] s;
     		s = Integer.toString(n).split("");
     		for(int i = 0; i<m; i++) {
     			System.out.print(s[i%s.length]);
     		}
     	}
+    	
     	else {
     		for(int i = 0 ; i<n; i++) {
     			System.out.print(n);
