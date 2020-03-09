95365d73e886626ee82260720aaf2d9cc82b0cd5
HyeonJooo <58289478+HyeonJooo@users.noreply.github.com>
2020-03-09 12:07:12 +0900

    고려대는 사랑입니다

---
+++ BAEKJOON/알고리즘대회/고대/11942.java
@@ -0,0 +1,9 @@
+//2016 고려대학교 프로그래밍 경시대회 1번
+//백준 알고리즘 11942번 : 고려대는 사랑입니다
+import java.io.*;
+import java.util.*;
+public class Main {	
+    public static void main(String[] args) throws IOException {
+       System.out.println("고려대학교");
+    }
+}
