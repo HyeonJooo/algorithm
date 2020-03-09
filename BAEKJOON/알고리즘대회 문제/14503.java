a659ff8e304afec77004c67291834b0340d1f9b5
HyeonJooo <58289478+HyeonJooo@users.noreply.github.com>
2020-03-09 11:59:26 +0900

    로봇청소기

--- BAEKJOON/알고리즘대회 문제/14503.java
+++ BAEKJOON/알고리즘대회 문제/14503.java
@@ -1,23 +1,18 @@
-//백준 알고리즘 14503번 : 로봇청소기
+//백준 알고리즘 14503번 : 로봇 청소기
 import java.io.*;
 import java.util.*;
 public class Main {
-	static int n,m,r,c,d;
-	static int[][] arr;
-	static int[] dx = {-1,0,1,0};
-    static int[] dy = {0,1,0,-1};			
-	
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-       //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //StringTokenizer st = new StringTokenizer(br.readLine());
+        Scanner sc = new Scanner(System.in);       
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int r = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
         
-        n = sc.nextInt();
-        m = sc.nextInt();
-        arr = new int[n][m];
-        r = sc.nextInt();
-        c = sc.nextInt();
-        d = sc.nextInt();
+        int[][] arr = new int[n][m];
+    	int[] dx = {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
         
         for(int i = 0 ; i<n; i++) {
            for(int j = 0 ; j<m; j++) {
@@ -28,12 +23,12 @@ public class Main {
         
         while(true) {
         	if(arr[r][c] == 0)
-        		arr[r][c] = 2;
+        		arr[r][c] = 2; //현재위치 청소
         	
-        	else {
+        	else {//네 방향 모두 청소 되어있거나, 벽인 경우
         		if(arr[r+1][c] != 0 && arr[r][c+1] != 0 && arr[r-1][c] != 0 && arr[r][c-1] != 0) {
-        			if(arr[r-dx[d]][c-dy[d]] == 1)
-        				break;
+        			if(arr[r-dx[d]][c-dy[d]] == 1) //뒤쪽 방향이 벽이라 후진도 할 수 없는 경우
+        				break;//작동을 멈춤
         			else {
         				r = r-dx[d];
         				c = c-dy[d];
